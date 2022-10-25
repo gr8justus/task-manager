99a93fc88894cf997ba8f911b96748203a077e28
Oluwatobi Oladoye <oladoyeolamide@gmail.com>
2022-10-25 15:17:53 +0100

    Picture upload feature added.

--- src/models/user.js
+++ src/models/user.js
@@ -49,7 +49,10 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true
         }
-    }]
+    }],
+    avatar: {
+        type: Buffer
+    }
 }, {
     timestamps: true
 });
@@ -99,6 +102,7 @@ userSchema.methods.toJSON = function () {
     // values of parameters to be hidden
     delete userObject.password;
     delete userObject.tokens;
+    delete userObject.avatar;
 
     return userObject;
 }
--- src/router/user.js
+++ src/router/user.js
@@ -2,8 +2,24 @@
 
 // Required modules
 import express from 'express';
+import multer from 'multer';
+import sharp from 'sharp';
 import { auth, User, Task } from '../component.js';
 const userRouter = express.Router();
+
+// Config multer
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)/)) {
+            return cb(new Error('You can only upload jpg, jpeg, and png format'))
+        }
+
+        cb(undefined, true);
+    }
+});
 
 // create / write into db
 userRouter.post('/users', async (req, res) => {
@@ -93,6 +109,7 @@ userRouter.patch('/users/me', auth, async (req, res) => {
     }
 });
 
+// Delete user and her tasks
 userRouter.delete('/users/me', auth, async (req, res) => {
     try {
         await User.deleteOne({_id: req.id});
@@ -101,6 +118,39 @@ userRouter.delete('/users/me', auth, async (req, res) => {
     } catch (e) {
         res.status(500).send(e.message);
     }
+});
+
+// upload avatar
+userRouter.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
+    const user = await User.findById(req.id);
+    user.avatar = buffer;
+    await user.save();
+    res.send();
+}, (e, req, res, next) => res.status(400).send({error: e.message}));
+
+// View avatar
+userRouter.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user|!user.avatar) {
+            throw new Error();
+        }
+
+        res.set('Content-Type', 'image/png');
+        res.send(user.avatar);
+    } catch (e) {
+        res.status(404).send();
+    }
+});
+
+// Delete avatar
+userRouter.delete('/users/me/avatar', auth, async (req, res) => {
+    const user = await User.findById(req.id);
+    user.avatar = undefined;
+    await user.save();
+    res.send();
 });
 
 export { userRouter };
